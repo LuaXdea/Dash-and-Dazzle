8d508ce5f9591dbb243d78e0be62add2ea777384
LuaXdea <144655350+LuaXdea@users.noreply.github.com>
2026-02-24 18:13:19 -0400

    Update CamFlow version and improve camera smoothing

--- scripts/Dash & Dazzle/CamFlow.lua
+++ scripts/Dash & Dazzle/CamFlow.lua
@@ -1,4 +1,4 @@
--- | Dash & Dazzle • CamFlow v1.6.1[Fix] [Return] |
+-- | Dash & Dazzle • CamFlow v1.6.5[Fix] [Return] |
 function onUpdate()
     CamFlow = getProperty('CamFlow')
     CustomCam = getProperty('CustomCam')
@@ -19,19 +19,32 @@ function onUpdate()
 
     directionOffsets = getProperty('directionOffsets')
 end
-function onUpdatePost()
+local smoothSpeed = 25
+local camOffsetX = 0
+local camOffsetY = 0
+
+function smooth(current,target,elapsed,speed)
+    local t = 1 - math.exp(-speed * elapsed)
+    return current + (target - current) * t
+end
+function onUpdatePost(elapsed)
     local BaseX = gfSection and camX_gf or (mustHitSection and camX_player or camX_opponent)
     local BaseY = gfSection and camY_gf or (mustHitSection and camY_player or camY_opponent)
-    if CustomCam then callMethod('camFollow.setPosition',{BaseX,BaseY}) end
-    local Offsets = IndividualOffsets and (gfSection and offset_gf or mustHitSection and offset_player or offset_opponent) or GeneralOffset
-    local offsetX,offsetY = 0,0
+    if CustomCam then
+        callMethod('camFollow.setPosition',{BaseX,BaseY})
+    end
+    local targetX,targetY = 0, 0
     if CamFlow then
+        local Offsets = IndividualOffsets and (gfSection and offset_gf or mustHitSection and offset_player or offset_opponent)
+            or GeneralOffset
         for i = 0,7 do
             if getPropertyFromGroup('strumLineNotes',i,'animation.curAnim.name') == 'confirm' then
-                offsetX = offsetX + directionOffsets[i + 1][1] * Offsets
-                offsetY = offsetY + directionOffsets[i + 1][2] * Offsets
+                targetX = targetX + directionOffsets[i + 1][1] * Offsets
+                targetY = targetY + directionOffsets[i + 1][2] * Offsets
             end
         end
     end
-    callMethod('camGame.targetOffset.set',{offsetX,offsetY})
+    camOffsetX = smooth(camOffsetX,targetX,elapsed,smoothSpeed)
+    camOffsetY = smooth(camOffsetY,targetY,elapsed,smoothSpeed)
+    callMethod('camGame.targetOffset.set',{camOffsetX,camOffsetY})
 end
